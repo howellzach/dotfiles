92faf11cba6ad10c24a556c4ec362a1307633ee7
Zach Howell <zach.howell@proton.me>
2023-01-31 21:24:54 +1300

    Add nvim

---
+++ .config/nvim/lua/howellzach/init.lua
@@ -0,0 +1,9 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("howellzach.packer")
+require("howellzach.remap")
+require("howellzach.set")
+require("howellzach.numbertoggle")
+
