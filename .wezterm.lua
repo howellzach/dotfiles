69e4b3639739d4ac379173459313d29a7c65dd82
Zach Howell <zach.howell@proton.me>
2024-01-22 10:24:59 +1300

    Add window transparency

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,4 +20,9 @@ config.font_size = 13
 -- config.freetype_load_target = 'Light'
 -- config.freetype_render_target = 'HorizontalLcd'
 
+config.window_background_opacity = 0.96
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+
 return config
