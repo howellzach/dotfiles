242be8bc0c6219ecd97a9979f6b70af353b14cd8
Zach Howell <zach.howell@proton.me>
2024-01-22 10:30:04 +1300

    slightly less transparent

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,7 +20,7 @@ config.font_size = 13
 -- config.freetype_load_target = 'Light'
 -- config.freetype_render_target = 'HorizontalLcd'
 
-config.window_background_opacity = 0.96
+config.window_background_opacity = 0.98
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
